3caeffd87fe120e3aa441e2964f60d77bc66db84
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-06-27 21:03:01 -0500

    m

--- src/Router.php
+++ src/Router.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of HNova/api.
+ *
+ * (c) Heiler Nova <https://github.com/heilernova>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace HNova\Rest;
 
 class Router
